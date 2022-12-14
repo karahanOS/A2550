90f29f071fadb27de67ace7220f22a0738d11e4b
KARAHAN <KARAHAN>
2022-12-14 14:22:02 +0300

    I`ve changed list that I have pulled from astro2.py

--- fit.py
+++ fit.py
@@ -1,12 +1,12 @@
-from astro2 import sur_bri_list
-from astro2 import radius_list
+from astro2 import sur_bri_error_list
+from astro2 import radius_error_list
 import numpy as np # import numpy to use in our code
 import matplotlib.pyplot as plt # import matplotlib to create our plot
 from scipy.optimize import curve_fit
 
 
-y_data = np.asarray(sur_bri_list)
-x_data = np.asarray(radius_list)
+y_data = np.asarray(sur_bri_error_list)
+x_data = np.asarray(radius_error_list)
 
 def beta(r, s0, rc, b):
     return s0 * (1.0 + (r/rc)**2.0)**(-3*b + 0.5)
@@ -20,7 +20,7 @@ fit_B = parameters[1]
 print(fit_A)
 print(fit_B)
 
-fit_y = beta(x_data, fit_A, fit_B)
+fit_y = beta(x_data, y_data, fit_A, fit_B)
 plt.plot(x_data, y_data, 'o', label='data')
 plt.plot(x_data, fit_y, '-', label='fit')
 plt.legend()
